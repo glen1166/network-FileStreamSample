23bcd14f5490e9e798073d81fdefe26b43b20382
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-25 00:05:51 +0800

    StreamReader

--- FileStreamSample/FileStreamSample/Program.cs
+++ FileStreamSample/FileStreamSample/Program.cs
@@ -8,58 +8,30 @@ namespace FileStreamSample
     {
         static void Main(string[] args)
         {
-            int count;
-            byte[] byteArray;
-            char[] charArray;
-            UnicodeEncoding uniEncoding = new UnicodeEncoding();
+            ReadText();
+        }
 
-            // Create the data to write to the stream.
-            byte[] firstString = uniEncoding.GetBytes(
-                "Invalid file path characters are: ");
-            byte[] secondString = uniEncoding.GetBytes(
-                Path.GetInvalidPathChars());
+        async static void ReadText()
+        {
+            string filename = @"E:\temp\test.txt";
+            char[] result;
+            StringBuilder builder = new StringBuilder();
 
-            using (MemoryStream memStream = new MemoryStream(100))
+            using (StreamReader reader = File.OpenText(filename))
             {
-                // Write the first string to the stream.
-                memStream.Write(firstString, 0, firstString.Length);
-
-                // Write the second string to the stream, byte by byte.
-                count = 0;
-                while (count < secondString.Length)
-                {
-                    memStream.WriteByte(secondString[count++]);
-                }
-
-                // Write the stream properties to the console.
-                Console.WriteLine(
-                    "Capacity = {0}, Length = {1}, Position = {2}\n",
-                    memStream.Capacity.ToString(),
-                    memStream.Length.ToString(),
-                    memStream.Position.ToString());
-
-                // Set the position to the beginning of the stream.
-                memStream.Seek(0, SeekOrigin.Begin);
-
-                // Read the first 20 bytes from the stream.
-                byteArray = new byte[memStream.Length];
-                count = memStream.Read(byteArray, 0, 20);
-
-                // Read the remaining bytes, byte by byte.
-                while (count < memStream.Length)
-                {
-                    byteArray[count++] =
-                        Convert.ToByte(memStream.ReadByte());
-                }
-
-                // Decode the byte array into a char array
-                // and write it to the console.
-                charArray = new char[uniEncoding.GetCharCount(
-                    byteArray, 0, count)];
-                uniEncoding.GetDecoder().GetChars(
-                    byteArray, 0, count, charArray, 0);
-                Console.WriteLine(charArray);
+                result = new char[reader.BaseStream.Length];
+                await reader.ReadAsync(result, 0, (int)reader.BaseStream.Length);
             }
+
+            foreach (char c in result)
+            {
+                if (char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
+                {
+                    builder.Append(c);
+                }
+            }
+
+            Console.WriteLine(builder.ToString());
         }
     }
 }
