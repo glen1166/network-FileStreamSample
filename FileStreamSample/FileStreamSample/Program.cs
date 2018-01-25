376bef9aea5dc1849183c23e3106619d2797190e
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-25 22:07:47 +0800

    StringWriter

--- FileStreamSample/FileStreamSample/Program.cs
+++ FileStreamSample/FileStreamSample/Program.cs
@@ -11,27 +11,68 @@ namespace FileStreamSample
 
         static void Main(string[] args)
         {
-            // Get the directories currently on the C drive.
-            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+            string textReaderText = "TextReader is the abstract base " +
+            "class of StreamReader and StringReader, which read " +
+            "characters from streams and strings, respectively.\n\n" +
 
-            // Write each directory name to a file.
-            using (StreamWriter sw = new StreamWriter(fileName))
+            "Create an instance of TextReader to open a text file " +
+            "for reading a specified range of characters, or to " +
+            "create a reader based on an existing stream.\n\n" +
+
+            "You can also use an instance of TextReader to read " +
+            "text from a custom backing store using the same " +
+            "APIs you would use for a string or a stream.\n\n";
+
+            Console.WriteLine("Original text:\n\n{0}", textReaderText);
+
+            // From textReaderText, create a continuous paragraph 
+            // with two spaces between each sentence.
+            string aLine, aParagraph = null;
+            StringReader strReader = new StringReader(textReaderText);
+            while (true)
             {
-                foreach (DirectoryInfo dir in cDirs)
+                aLine = strReader.ReadLine();
+                if (aLine != null)
                 {
-                    sw.WriteLine(dir.Name);
+                    aParagraph = aParagraph + aLine + " ";
+                }
+                else
+                {
+                    aParagraph = aParagraph + "\n";
+                    break;
                 }
             }
+            Console.WriteLine("Modified text:\n\n{0}", aParagraph);
 
-            // Read and show each line from the file.
-            string line = "";
-            using (StreamReader sr = new StreamReader(desFileName))
+            // Re-create textReaderText from aParagraph.
+            int intCharacter;
+            char convertedCharacter;
+            StringWriter strWriter = new StringWriter();
+            strReader = new StringReader(aParagraph);
+            while (true)
             {
-                while ((line = sr.ReadLine()) != null)
+                intCharacter = strReader.Read();
+
+                // Check for the end of the string 
+                // before converting to a character.
+                if (intCharacter == -1) break;
+
+                convertedCharacter = Convert.ToChar(intCharacter);
+                if (convertedCharacter == '.')
                 {
-                    Console.WriteLine(line);  
+                    strWriter.Write(".\n\n");
+
+                    // Bypass the spaces between sentences.
+                    strReader.Read();
+                    strReader.Read();
+                }
+                else
+                {
+                    strWriter.Write(convertedCharacter);
                 }
             }
+            Console.WriteLine("\nOriginal text:\n\n{0}",
+                strWriter.ToString());
         }
     }
 }
